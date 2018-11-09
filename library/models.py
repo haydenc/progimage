050119d643bf6040e352c3be36ba22180912f144
Hayden Crocker <hayden@debtxplained.com>
2018-11-09 13:28:07 +0000

    Adding stored image to django admin (to delete)

--- library/models.py
+++ library/models.py
@@ -7,3 +7,6 @@ class StoredImage(UpdateInfoBaseModel):
     # Block cascading deletion of stored images because we need to delete the image from the filesystem
     owner = models.ForeignKey("auth.User", on_delete=models.PROTECT)
     original = models.ImageField(upload_to="stored_images")
+
+    def __str__(self):
+        return "Image uploaded by {} at {}".format(self.owner, self.createtime)
