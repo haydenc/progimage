e1b60d01614f66c1aebf603f81cf0fd1c010d083
Hayden Crocker <hayden@debtxplained.com>
2018-11-09 12:48:23 +0000

    Created basic stored image model with owner and audit

--- library/models.py
+++ library/models.py
@@ -1,1 +1,8 @@
 from django.db import models
+from django_tools.models import UpdateInfoBaseModel
+
+
+class StoredImage(UpdateInfoBaseModel):
+    # Block cascading deletion of stored images because we need to delete the image from the filesystem
+    owner = models.ForeignKey("auth.User", on_delete=models.PROTECT)
+    original = models.ImageField()
